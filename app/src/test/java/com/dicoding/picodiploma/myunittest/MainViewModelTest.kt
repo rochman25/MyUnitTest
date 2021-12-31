219dfdeddcecd62f4b2c47c7b3491f4bd7bfc428
Zaenurrochman <zaenur.rochman98@gmail.com>
2022-01-01 01:32:42 +0700

    add mock test

--- app/src/test/java/com/dicoding/picodiploma/myunittest/MainViewModelTest.kt
+++ app/src/test/java/com/dicoding/picodiploma/myunittest/MainViewModelTest.kt
@@ -4,7 +4,7 @@ import org.junit.Assert.*
 import org.junit.Before
 
 import org.junit.Test
-import org.mockito.Mockito.mock
+import org.mockito.Mockito.*
 
 class MainViewModelTest {
 
@@ -50,5 +50,29 @@ class MainViewModelTest {
         mainViewModel.save(dummyWidth, dummyLength, dummyHeight)
         val volume = mainViewModel.getSurfaceArea()
         assertEquals(dummySurfaceArea, volume, 0.0001)
+    }
+
+    @Test
+    fun testMockVolume(){
+        `when`(mainViewModel.getVolume()).thenReturn(dummyVolume)
+        val volume = mainViewModel.getVolume()
+        verify(cuboidModel).getVolume()
+        assertEquals(dummyVolume, volume, 0.0001)
+    }
+
+    @Test
+    fun testMockCircumference(){
+        `when`(mainViewModel.getCircumference()).thenReturn(dummyCircumference)
+        val circumference = mainViewModel.getCircumference()
+        verify(cuboidModel).getCircumference()
+        assertEquals(dummyCircumference, circumference, 0.0001)
+    }
+
+    @Test
+    fun testMockSurfaceArea(){
+        `when`(mainViewModel.getSurfaceArea()).thenReturn(dummySurfaceArea)
+        val surfaceArea = mainViewModel.getSurfaceArea()
+        verify(cuboidModel).getSurfaceArea()
+        assertEquals(dummySurfaceArea, surfaceArea, 0.0001)
     }
 }
