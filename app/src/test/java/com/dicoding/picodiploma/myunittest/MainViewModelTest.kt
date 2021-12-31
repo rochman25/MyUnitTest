084df37a2f1cdd01039878a4683e3f16f40c93df
Zaenurrochman <zaenur.rochman98@gmail.com>
2022-01-01 01:22:40 +0700

    add unit test dependency and volume unit test

---
+++ app/src/test/java/com/dicoding/picodiploma/myunittest/MainViewModelTest.kt
@@ -0,0 +1,54 @@
+package com.dicoding.picodiploma.myunittest
+
+import org.junit.Assert.*
+import org.junit.Before
+
+import org.junit.Test
+import org.mockito.Mockito.mock
+
+class MainViewModelTest {
+
+    private lateinit var mainViewModel: MainViewModel
+    private lateinit var cuboidModel: CuboidModel
+
+    private val dummyLength = 12.0
+    private val dummyWidth = 7.0
+    private val dummyHeight = 6.0
+
+    private val dummyVolume = 504.0
+    private val dummyCircumference = 100.0
+    private val dummySurfaceArea = 396.0
+
+    @Before
+    fun before() {
+        cuboidModel = mock(CuboidModel::class.java)
+        mainViewModel = MainViewModel(cuboidModel)
+    }
+
+    @Test
+    fun testVolume() {
+        cuboidModel = CuboidModel()
+        mainViewModel = MainViewModel(cuboidModel)
+        mainViewModel.save(dummyWidth, dummyLength, dummyHeight)
+        val volume = mainViewModel.getVolume()
+        assertEquals(dummyVolume, volume, 0.0001)
+    }
+
+    @Test
+    fun testCircumference() {
+        cuboidModel = CuboidModel()
+        mainViewModel = MainViewModel(cuboidModel)
+        mainViewModel.save(dummyWidth, dummyLength, dummyHeight)
+        val volume = mainViewModel.getCircumference()
+        assertEquals(dummyCircumference, volume, 0.0001)
+    }
+
+    @Test
+    fun testSurfaceArea() {
+        cuboidModel = CuboidModel()
+        mainViewModel = MainViewModel(cuboidModel)
+        mainViewModel.save(dummyWidth, dummyLength, dummyHeight)
+        val volume = mainViewModel.getSurfaceArea()
+        assertEquals(dummySurfaceArea, volume, 0.0001)
+    }
+}
